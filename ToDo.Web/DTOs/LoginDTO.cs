b479f593b79a322edb4e05299a433641c3a5bbeb
OneWay2Go <uchqunov.m@yandex.com>
2026-07-23 15:14:29 +0500

    some auth dtos

---
+++ ToDo.Web/DTOs/LoginDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace ToDo.Web;
+
+public class LoginDTO
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
---
+++ ToDo.Web/DTOs/RegisterDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDo.Web;
+
+public class RegisterDTO
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string ReTypePassword { get; set; }
+}
